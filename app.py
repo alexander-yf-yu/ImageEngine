d870c85b320dbb3db7e4cb37fbdfffd57d6ce26b
Alex Yu <alexander.yf.yu@gmail.com>
2022-05-14 18:53:32 -0400

    run with gunicorn

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
