bd96fe3884ce4122fbc68fa13c18b4fae7b62c23
Alex Yu <alexander.yf.yu@gmail.com>
2022-05-22 09:24:34 -0400

    remove google version, archival tag code

--- models.py
+++ models.py
@@ -2,13 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import enum
 from sqlalchemy import Enum
+from datetime import datetime
 
-class Tag(enum.Enum):
-    art = "art"
-    people = "people"
-    nature = "nature"
-    tech = "tech"
-    misc = "misc"
 
 db = SQLAlchemy()
 
@@ -16,7 +11,18 @@ class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(80), unique=True, nullable=False)
     description = db.Column(db.String(120), unique=False, nullable=True)
-    # tag = db.Column(db.Enum(('art', 'people', 'nature', 'tech', 'misc')))
+    pub_date = db.Column(db.Date, nullable=False, default=datetime.today)
+
+    # tag_id = db.Column(db.Integer, db.ForeignKey('tag.id'), nullable=False)
+    # tag = db.relationship('Tag', backref=db.backref('images', lazy=True))
 
     def __repr__(self):
         return '<Image %r>' % self.filename
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return '<Tag %r>' % self.name
